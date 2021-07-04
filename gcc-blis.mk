332ee2070933624025d83e62b6d7fe2bd7ddeaf6
Alejandro Gallo <aamsgallo@gmail.com>
2021-07-04 17:23:35 +0200

    Fix blis building

--- gcc-blis.mk
+++ gcc-blis.mk
@@ -3,7 +3,7 @@ OPTIONS = -fopenmp
 BLIS_ARCH ?= intel64
 #BLIS_ARCH ?= haswell
 
-BLIS_LIB_PATH = blis/lib/$(BLIS_ARCH)
+BLIS_LIB_PATH = ./blis/lib/$(BLIS_ARCH)
 LIBS_STATIC = -L$(BLIS_LIB_PATH) -lblis
 
 DEFINES += -DHAS_GCC -DHAS_BLIS -DBLIS_ARCH=$(BLIS_ARCH)
@@ -11,6 +11,6 @@ INCLUDES += -I./blis/include/$(BLIS_ARCH)/
 
 blis: $(BLIS_LIB_PATH)
 
-./blis/lib/%/libblis.a:
+./blis/lib/%:
 	git clone https://github.com/flame/blis
 	cd blis && ./configure $* && $(MAKE)
